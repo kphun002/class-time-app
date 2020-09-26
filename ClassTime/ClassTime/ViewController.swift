fd1c78c7719f69f81d08d4781b215bf50378abe6
Reiner Gonzalez <reinergonzalez@MacBook-Pro.local>
2020-09-26 01:16:17 -0400

    Base project push

---
+++ ClassTime/ClassTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassTime
+//
+//  Created by Reiner Gonzalez on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

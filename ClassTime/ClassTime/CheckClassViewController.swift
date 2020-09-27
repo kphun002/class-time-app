52ef16769b7afe1080fca2a743e3215a0a591d35
Karol Phung <phungkarol@gmail.com>
2020-09-26 23:37:59 -0400

    almost done

--- ClassTime/ClassTime/CheckClassViewController.swift
+++ ClassTime/ClassTime/CheckClassViewController.swift
@@ -9,21 +9,37 @@ import UIKit
 
 class CheckClassViewController: UIViewController {
 
+    @IBOutlet weak var courseCode: UITextField!
+    @IBOutlet weak var hours: UILabel!
+    @IBOutlet weak var addToSchedule: UIButton!
+    
+    var selectedCourses: [String:Int] = [:]
+    
+    let myCourseInfoModel = CourseInfoModel.sharedCourseInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func tapOnSearchCourse(_ sender: Any) {
+        hours.text = String(myCourseInfoModel.averageForCourse(courseName: courseCode.text!))
+    }
+    
+    @IBAction func tapOnAddToSchedule(_ sender: Any) {
+        selectedCourses[courseCode.text!] = Int(hours.text!)
+        print(selectedCourses)
+    }
+    
     /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let scheduleViewController = segue.destination as? ScheduleViewController
+        scheduleViewController?.coursesInSchedule = selectedCourses
+    }
 
 }
---
+++ ClassTime/ClassTime/CourseViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CourseViewCell.swift
+//  ClassTime
+//
+//  Created by Karol Phung on 9/26/20.
+//
+
+import UIKit
+
+class CourseViewCell: UITableViewCell {
+
+    @IBOutlet weak var courseCode: UILabel!
+    @IBOutlet weak var hours: UILabel!
+    @IBOutlet weak var delete: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
